a331a3428d0ad34999af0f2fc9663b2adf70e251
aogu <aogu89goo415@gmail.com>
2022-10-29 19:14:01 +1100

    Conditions

--- 1-GO-PROJECT-TUTORIAL/tutorial.go
+++ 1-GO-PROJECT-TUTORIAL/tutorial.go
@@ -5,16 +5,47 @@ import "fmt"
 func main(){
 	fmt.Println("Welcom to my quiz game!")
 
-	fmt.Printf("Enter\nTom your name: ")
+	fmt.Printf("Enter\n your name: ")
 	var name string
 	fmt.Scan(&name)
 
-	fmt.Printf("Hello, %v, welcome to the game!", name)
+	fmt.Printf("Hello, %v, welcome to the game!\n", name)
 
 	fmt.Printf("Enter your age: ")
 	var age uint
 	fmt.Scan(&age)
 
-	// fmt.Println(age >= )
+	if age >= 10{
+		fmt.Println("Yay you can play!")
+	}else{
+		fmt.Println("You can't play!")
+		return 
+	}
 
+	fmt.Println("What is better, The RTX 3080 or RTX 3090? ")
+	var answer string
+	var answer2 string
+	fmt.Scan(&answer, &answer2)
+
+
+
+	if answer + " " + answer2 == "RTX 3090"{
+		fmt.Println("Correct!")
+	}else if answer + " " + answer2 == "rtx 3090"{
+		fmt.Println("Correct!")
+	}else if answer + " " + answer2 == "rtX 3090"{
+		fmt.Println("Correct!")
+	}else{
+		fmt.Println("Incorrect!")
+	}
+
+	fmt.Printf("How many cores does the Ryzen 9 3900x have")
+	var corse uint
+	fmt.Scan(&corse)
+
+	if corse == 12{
+		fmt.Println("Correct!")
+	}else{
+		fmt.Println("Incorrect")
+	}
 }
