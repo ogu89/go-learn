407bec96693494295a408cf68f7e9cc0cfbca686
aogu <aogu89goo415@gmail.com>
2022-11-02 15:10:26 +1100

    createBook post

--- 2-GO-API-TUTORIAL/main.go
+++ 2-GO-API-TUTORIAL/main.go
@@ -24,9 +24,21 @@ func getBooks(c *gin.Context){
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func createBook(c *gin.Context){
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil{
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
 func main(){
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
 	router.Run("localhost:8080")
 
 }
