bc66c56c5c99461884c22214e24aa711bc83890b
aogu <aogu89goo415@gmail.com>
2022-11-05 18:18:16 +1100

    Movie & Director struct

---
+++ 6-GO-MOVIES-CRUD/main.go
@@ -0,0 +1,28 @@
+package main
+
+// "encoding/json"
+// "fmt"
+// "log"
+// "math/rand"
+// "net/http"
+// "strconv"
+
+// "github.com/gorilla/mux"
+
+type Movie struct{
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct{
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var movies []Movie
+
+// func main(){
+// 	r := mux.NewRouter()
+// }
