46ac0713642d1af90d3bfe9f3982c7892a9beef1
aogu <aogu89goo415@gmail.com>
2022-11-07 17:50:53 +1100

    pointers, strings-and-runes

---
+++ 3-GO-BY-EXAMPLE/strings-and-runes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main(){
+
+	const s = "สวัสดี"
+	const k = "slkjrwerj;we"
+
+
+	fmt.Println("Len:", len(s))
+
+
+	for i := 0; i < len(s); i++{
+		fmt.Printf("%x ", s[i])
+	}
+	fmt.Println()
+
+
+	fmt.Println("Rune count:", utf8.RuneCountInString(s))
+
+
+	for idx, runeValue := range s{
+		fmt.Printf("%#U starts at %d\n", runeValue, idx)
+	}
+}
+
