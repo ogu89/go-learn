62f2d8a23e08dfec851a0dc1e311a218105e1f52
aogu <aogu89goo415@gmail.com>
2022-11-08 19:26:20 +1100

    POST request

--- 7-TODO-APPLICATION/server/main.go
+++ 7-TODO-APPLICATION/server/main.go
@@ -7,14 +7,44 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct{
+	ID 		int `json:"id"`
+	Title 	string `json:"title"`
+	Done 	bool `json:"done"`
+	Body 	string `json:"body"`
+}
+
+
 func main(){
 	fmt.Println("Hello")
 
 	app := fiber.New()
 
+	todos := []Todo{}
+
+
 	app.Get("/healthcheck", func(c *fiber.Ctx) error{
 		return c.SendString("OK")
 	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx)  error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil{
+			fmt.Println("klds;jdflks")
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
+
+
+	})
+
+
 
 	log.Fatal(app.Listen(":4000"))
 }
