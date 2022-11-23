6f9da4a971cab068fda4f8f2226eb1ab88a23e23
aogu <aogu89goo415@gmail.com>
2022-11-23 13:36:18 +0900

    change cors settings

--- 7-TODO-APPLICATION/client/src/App.tsx
+++ 7-TODO-APPLICATION/client/src/App.tsx
@@ -1,13 +1,20 @@
 // import { Box } from "@mantine/core"
+import "./App.css";
+import useSWR from "swr";
 
+export const ENDPOINT = "http://localhost:4000"
+
+const fetcher = (url:string) => fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
 
 function App() {
+ 
+
+  const {data, mutate} = useSWR("api/todos", fetcher);
 
   return (
     <div >
-      Hello
+      {JSON.stringify(data)}
     </div>
-    
   )
 }
 
--- 7-TODO-APPLICATION/server/main.go
+++ 7-TODO-APPLICATION/server/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type Todo struct{
@@ -19,6 +20,11 @@ func main(){
 	fmt.Println("Hello")
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://127.0.0.1:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	todos := []Todo{}
 
