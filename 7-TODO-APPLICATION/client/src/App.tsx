4ff4b93633865693761f3b71ea70ec47576947f7
aogu <aogu89goo415@gmail.com>
2022-11-23 15:59:49 +0900

    toggle todo

--- 7-TODO-APPLICATION/client/src/App.tsx
+++ 7-TODO-APPLICATION/client/src/App.tsx
@@ -2,29 +2,61 @@
 import "./App.css";
 import useSWR from "swr";
 import AddTodo from "./components/AddTodo";
+import { List, ThemeIcon } from "@mantine/core";
+import { CheckCircleFillIcon } from "@primer/octicons-react";
 
-export interface Todo{
+export interface Todo {
   id: number;
   title: string;
   body: string;
-  done: boolean
+  done: boolean;
 }
 
-export const ENDPOINT = "http://localhost:4000"
+export const ENDPOINT = "http://localhost:4000";
 
-const fetcher = (url:string) => fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
+const fetcher = (url: string) =>
+  fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
 
 function App() {
- 
+  const { data, mutate } = useSWR<Todo[]>("api/todos", fetcher);
 
-  const {data, mutate} = useSWR<Todo[]>("api/todos", fetcher);
+
+  async function markTodoAsDone(id:number){
+    const updated = await fetch(`${ENDPOINT}/api/todos/${id}/done`, {
+      method: "PATCH",
+    }).then((r) => r.json());
+
+    mutate(updated)
+  }
 
   return (
-    <div >
-      {JSON.stringify(data)}
-      <AddTodo mutate={mutate}/>
+    <div>
+      <List spacing="xs" size="sm" mb={12} center>
+        {data?.map((todo) => {
+          return (
+            <List.Item 
+            onClick={() => markTodoAsDone(todo.id)}
+            key={`todo_list__${todo.id}`}
+            icon={
+              todo.done ? (
+                <ThemeIcon color="teal" size={24} radius="xl">
+                  <CheckCircleFillIcon size={20} />
+                </ThemeIcon>
+              ) : (
+                <ThemeIcon color="gray" size={24} radius="xl">
+                  <CheckCircleFillIcon size={20} />
+                </ThemeIcon>
+              )
+            }
+            >
+              {todo.title} 
+            </List.Item>
+          );
+        })}
+      </List>
+      <AddTodo mutate={mutate} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
