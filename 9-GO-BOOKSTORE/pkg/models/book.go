8dfe035089acfe6a763a41923eadc3841edef1ac
aogu <aogu89goo415@gmail.com>
2023-01-10 20:35:45 +0900

    book.go

---
+++ 9-GO-BOOKSTORE/pkg/models/book.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/ogu89/9-go-bookstore/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct{
+	gorm.model
+	Name string `gorm:""json:"name"`
+	Author string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init(){
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
